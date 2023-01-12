0d691857a7c844bfc641989fe69827908b81536e
Patrik <patrik@qmusicplayer.se>
2023-01-13 00:57:33 +0100

    moved h1 into react and fixed styling issue

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
 root.render(
 	<React.StrictMode>
+		<h1 id="title" className="center">Countries</h1>
 		<CountrySearch />
 	</React.StrictMode>
 );
